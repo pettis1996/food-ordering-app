0d4c9e6c58de85e04cfd405e1b3cf05cef3f9bd0
Paris Pettis <pettis_paris@hotmail.com>
2024-02-19 01:00:15 +0200

    minor adjustments to home menu

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <main className="max-w-4xl mx-auto border p-4">
+        <main className="max-w-4xl mx-auto p-4">
           {children}
         </main>
       </body>
--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -3,11 +3,11 @@ import Image from "next/image"
 export default function HomeMenu() {
     return (
         <section className="">
-            <div className="absolute left-0 right-0 w-full">
-                <div className="h-44 w-48 absolute -left-12 -top-12">
+            <div className="absolute h-full left-0 right-0">
+                <div className="h-44 w-48 absolute -left-12 -top-[95px] -z-10">
                     <Image src={"/sallad1.png"} layout={"fill"} objectFit={"contain"} alt={"Salad 1"} />
                 </div>
-                <div className="h-44 w-48 absolute -right-12 -top-12">
+                <div className="h-44 w-48 absolute -right-12 -top-[200px] -z-10">
                     <Image src={"/sallad2.png"} layout={"fill"} objectFit={"contain"} alt={"Salad 2"} />
                 </div>
             </div>
