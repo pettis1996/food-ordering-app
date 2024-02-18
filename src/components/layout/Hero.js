674775236bab8a3d26199190429246f09d2b6975
Paris Pettis <pettis_paris@hotmail.com>
2024-02-19 00:22:06 +0200

    minor changes

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -1,12 +1,12 @@
 import Image from "next/image"
-import Right from "@/components/icons/right";
+import Right from "@/components/icons/Right";
 
 export default function Hero() {
     return (
-        <section className="grid grid-cols-2">
-            <div>    
-                <h1 className="text-4xl font-semibold">Everything is better with a Pizza!</h1>
-                <p className="my-4 text-gray-500">
+        <section className="hero">
+            <div className="py-12">    
+                <h1 className="text-4xl font-semibold">Everything<br /> is better<br /> with a <span className="text-primary">Pizza</span>!</h1>
+                <p className="my-6 text-gray-500 text-sm">
                     Pizza is the missing piece that makes every day complete, a simple yet so delicious joy in life.
                 </p>
                 <div className="flex gap-4 text-sm">
