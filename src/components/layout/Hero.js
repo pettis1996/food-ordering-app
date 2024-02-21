e578b0910c33bbe5630850e31a93da9076cb3650
Paris Pettis <pettis_paris@hotmail.com>
2024-02-21 07:02:26 +0200

    minor change

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -14,7 +14,7 @@ export default function Hero() {
                         Order Now
                         <Right />
                     </button>
-                    <button className="flex gap-2 py-2 text-gray-600 font-semibold">
+                    <button className="flex items-center gap-2 py-2 text-gray-600 font-semibold">
                         Learn More
                         <Right />    
                     </button>
