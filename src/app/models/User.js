c44438b507eb696636d650cfab8861f79a80dc00
Paris Pettis <pettis_paris@hotmail.com>
2024-02-28 07:32:12 +0200

    added hashing for user password

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
@@ -6,8 +7,15 @@ const userSchema = new mongoose.Schema({
         validate: (pass) => {
             if (!pass?.length || pass?.length < 5) {
                 new Error("Password must be at least 5 characters long.");
+                return false;
             }
         }},
 }, {timestamps: true});
+
+userSchema.post("validate", function (user) {
+    const pass = user.password;
+    const salt = bcrypt.genSaltSync(10);
+    user.password = bcrypt.hashSync(pass, salt);
+})
 
 export const User = mongoose.models?.User || mongoose.model("User", userSchema);
