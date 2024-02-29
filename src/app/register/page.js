a6b3c8a025c9a02625c005a1ccf9701b8a45c36b
Paris Pettis <pettis_paris@hotmail.com>
2024-02-29 19:48:27 +0200

    fixed register page and minor styling adjustments

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/navigation'
 import Image from "next/image";
 import { useState } from "react";
 import Alert from "@/components/layout/Alert"
+import Link from "next/link"
 
 export default function RegisterPage() {
     const router = useRouter();
@@ -48,11 +49,18 @@ export default function RegisterPage() {
                 <input required disabled={creatingUser} type="email" placeholder="email" value={email} onChange={ev => setEmail(ev.target.value)} />
                 <input required disabled={creatingUser} type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)} />
                 <button disabled={creatingUser} type="submit">Create Account</button>
-                <div className="border-b border-gray-300 text-center text-gray-500 my-4">or login with provider</div>
-                <button className="flex gap-4 justify-center">
-                    <Image src={"/google.png"} width={24} height={24} alt={"Google"} />
-                    Login with Google
-                </button>
+                <div className="border-b border-gray-300 text-center text-gray-500 my-4 text-sm">or Login with Provider</div>
+                <div className="flex flex-col gap-3">
+                    <button className="flex gap-4 justify-center">
+                        <Image src={"/google.png"} width={24} height={24} alt={"Google"} />
+                        Login with Google
+                    </button>
+                    <button className="flex gap-4 justify-center">
+                        <Image src={"/github.png"} width={24} height={24} alt={"GitHub"} />
+                        Login with GitHub
+                    </button>
+                </div>
+                <div className="border-t border-gray-300 text-center text-gray-500 my-4 pt-4">Already have an account? <Link className="underline text-gray-900" href={"/login"}>Login</Link></div>
             </form>
         </section>
     );
