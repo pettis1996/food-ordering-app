ff0dd750c20c7078b1848936982d733d00cbc868
Paris Pettis <pettis_paris@hotmail.com>
2024-02-29 11:00:42 +0200

    fixed redirect and error alert for register page

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -10,11 +10,13 @@ export default function RegisterPage() {
     const [creatingUser, setCreatingUser] = useState(false);
     const [userCreated, setUserCreated] = useState(false);
     const [redirectSeconds, setRedirectSeconds] = useState(5);
+    const [userExists, setUserExists] = useState(false);
 
     async function handleFormSubmit(ev) {
         ev.preventDefault();
         setCreatingUser(true);
         setUserCreated(false);
+        setUserExists(false);
         const res = await fetch("/api/register", {
             method: "POST",
             body: JSON.stringify({email, password}),
@@ -23,21 +25,14 @@ export default function RegisterPage() {
         if (!res.ok) {
             console.log(res.status);
             setCreatingUser(false);
+            setUserExists(true);
             return null;
         }
         setUserCreated(true);
         setCreatingUser(false);
         setTimeout(() => {
-            const interval = setInterval(() => {
-                console.log(redirectSeconds);
-                if (redirectSeconds > 0) {
-                    setRedirectSeconds(redirectSeconds - 1);
-                } else {
-                    clearInterval(interval);
-                    router.push("/");
-                }
-            }, 1000)
-        }, 1000);
+            router.push("/");
+        }, 2000);
     };
 
     return (
@@ -45,11 +40,20 @@ export default function RegisterPage() {
             <h1 className="text-center text-primary text-4xl">Register</h1>
             <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
                 {userCreated ? (
-                    <div className="bg-green-300/50 rounded-md p-5 font-semibold text-center my-5 flex justify-center items-center gap-3">
+                    <div className="bg-green-300/50 rounded-md p-5 font-semibold text-center my-5 flex flex-col justify-center items-center gap-3">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
                         </svg>
-                        <h6 className="text-sm">User Created Successfully!</h6>
+                        <span className="text-sm">User Created Successfully!</span>
+                    </div>
+                ): null}
+                {userExists ? (
+                    <div className="bg-red-300/50 rounded-md p-5 font-semibold text-center my-5 flex flex-col justify-center items-center gap-3">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" />
+                        </svg>
+                        <span className="text-sm">User Already Exists!</span>
+                        <span className="text-xs">Please try again using a different email address.</span>
                     </div>
                 ): null}
                 <input required disabled={creatingUser} type="email" placeholder="email" value={email} onChange={ev => setEmail(ev.target.value)} />
