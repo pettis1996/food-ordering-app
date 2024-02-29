e8d2b2256c4dd70c1f0d03969acd05777800d193
Paris Pettis <pettis_paris@hotmail.com>
2024-02-29 20:45:18 +0200

    register page fixes and setup for login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -9,25 +9,45 @@ import Link from "next/link"
 export default function LoginPage() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [authenticating, setAuthenticating] = useState(false);
+    const [error, setError] = useState(false);
 
     async function handleFormSubmit(ev) {
         ev.preventDefault();
+        setError(false);
+        setAuthenticating(true);
+        const res = await fetch("/api/login", {
+            method: "POST",
+            body: JSON.stringify({email, password}),
+            headers: {"Content-Type": "application/json"}
+        });
+        if (!res.ok) {
+            setError(true);
+            return null;
+        }
+        setAuthenticating(false);
+        setTimeout(() => {
+            router.push("/");
+        }, 1750);
     };
 
     return (
         <section className="mt-8">
             <h1 className="text-center text-primary text-4xl">Login</h1>
             <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
-                <input required disabled={false} type="email" placeholder="email" value={email} onChange={ev => setEmail(ev.target.value)} />
-                <input required disabled={false} type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)} />
-                <button disabled={false} type="submit">Login</button>
+                {error && (
+                    <Alert type="error" smallText="Oops, An error occured!" exSmallText="Please try again." />
+                )}
+                <input required disabled={authenticating} type="email" placeholder="email" value={email} onChange={ev => setEmail(ev.target.value)} />
+                <input required disabled={authenticating} type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)} />
+                <button disabled={authenticating} type="submit">Login</button>
                 <div className="border-b border-gray-300 text-center text-gray-500 my-4 text-sm">or Login with Provider</div>
                 <div className="flex flex-col gap-3">
-                    <button className="flex gap-4 justify-center">
+                    <button disabled={authenticating} className="flex gap-4 justify-center">
                         <Image src={"/google.png"} width={24} height={24} alt={"Google"} />
                         Login with Google
                     </button>
-                    <button className="flex gap-4 justify-center">
+                    <button disabled={authenticating} className="flex gap-4 justify-center">
                         <Image src={"/github.png"} width={24} height={24} alt={"GitHub"} />
                         Login with GitHub
                     </button>
