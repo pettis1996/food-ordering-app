2d60bfeed28552901f4076ec2652dd924942c33c
Paris Pettis <pettis_paris@hotmail.com>
2024-02-29 12:16:48 +0200

    minor styling adjustments for register page

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -9,7 +9,6 @@ export default function RegisterPage() {
     const [password, setPassword] = useState("");
     const [creatingUser, setCreatingUser] = useState(false);
     const [userCreated, setUserCreated] = useState(false);
-    const [redirectSeconds, setRedirectSeconds] = useState(5);
     const [userExists, setUserExists] = useState(false);
 
     async function handleFormSubmit(ev) {
@@ -29,10 +28,10 @@ export default function RegisterPage() {
             return null;
         }
         setUserCreated(true);
-        setCreatingUser(false);
         setTimeout(() => {
+            setCreatingUser(false);
             router.push("/");
-        }, 2000);
+        }, 1750);
     };
 
     return (
@@ -45,6 +44,7 @@ export default function RegisterPage() {
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
                         </svg>
                         <span className="text-sm">User Created Successfully!</span>
+                        <span className="text-xs">Please wait to be redirected to homepage.</span>
                     </div>
                 ): null}
                 {userExists ? (
