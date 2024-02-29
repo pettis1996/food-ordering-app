cbb5989cdaa38504a11b1c082946cf0ffb7959e9
Paris Pettis <pettis_paris@hotmail.com>
2024-02-29 20:14:41 +0200

    login page setup

---
+++ src/app/login/page.js
@@ -0,0 +1,39 @@
+"use client"
+import { useRouter } from 'next/navigation'
+import Image from "next/image";
+import { useState } from "react";
+import Alert from "@/components/layout/Alert"
+import Link from "next/link"
+
+
+export default function LoginPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function handleFormSubmit(ev) {
+        ev.preventDefault();
+    };
+
+    return (
+        <section className="mt-8">
+            <h1 className="text-center text-primary text-4xl">Login</h1>
+            <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+                <input required disabled={false} type="email" placeholder="email" value={email} onChange={ev => setEmail(ev.target.value)} />
+                <input required disabled={false} type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)} />
+                <button disabled={false} type="submit">Login</button>
+                <div className="border-b border-gray-300 text-center text-gray-500 my-4 text-sm">or Login with Provider</div>
+                <div className="flex flex-col gap-3">
+                    <button className="flex gap-4 justify-center">
+                        <Image src={"/google.png"} width={24} height={24} alt={"Google"} />
+                        Login with Google
+                    </button>
+                    <button className="flex gap-4 justify-center">
+                        <Image src={"/github.png"} width={24} height={24} alt={"GitHub"} />
+                        Login with GitHub
+                    </button>
+                </div>
+                <div className="border-t border-gray-300 text-center text-gray-500 my-4 pt-4">Don't have an account? <Link className="underline text-gray-900" href={"/register"}>Register</Link></div>
+            </form>
+        </section>
+    );
+}
